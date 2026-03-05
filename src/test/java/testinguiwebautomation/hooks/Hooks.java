ca66035f772e94de6ef161e20812adc0d5c74d3c
sastimaulii <sastimaulii17@gmail.com>
2026-03-05 16:52:56 +0700

    Update: Implement headless mode and GitHub Actions workflow

--- src/test/java/testinguiwebautomation/hooks/Hooks.java
+++ src/test/java/testinguiwebautomation/hooks/Hooks.java
@@ -5,6 +5,7 @@ import io.cucumber.java.Before;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Hooks {
     public static WebDriver driver;
@@ -12,7 +13,17 @@ public class Hooks {
     @Before
     public void openBrowser() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        //konfigurasi ChromeOptions
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // Menjalankan tanpa jendela UI
+        options.addArguments("--no-sandbox"); // Penting untuk environment Linux/Docker
+        options.addArguments("--disable-dev-shm-usage"); // Mencegah crash karena keterbatasan memori di server
+        options.addArguments("--window-size=1920,1080"); // Menentukan resolusi layar virtual
+
+        // options ChromeDriver
+        driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
     }
 
